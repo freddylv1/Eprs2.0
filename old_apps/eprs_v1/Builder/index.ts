03fbfa5b727edee1c3513a1e31938026bb6da681
freddylv1 <129158307+freddylv1@users.noreply.github.com>
2026-09-11 13:58:08 +0800

    Delete old_apps/eprs_v1 directory

--- old_apps/eprs_v1/Builder/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './exception_database';
-export * from './exception_resolver';
-export * from './vocabulary_builder';
-export * from './database_generator';
-export * from './workflow_processor';
